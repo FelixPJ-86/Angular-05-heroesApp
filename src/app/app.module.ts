8055a328e00240fb8ade5a49ca35bfa83647183f
Félix <felix.potente@alten.es>
2021-06-11 10:30:28 +0200

    Tema 13 Fin video "7. Heroes Backend - json-server" , "8. Heroes Service - Traer información de los héroes"  ,  "9. Interfaz Héroe" y "10. Material Card - Flex Layout"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ErrorPageComponent } from './shared/error-page/error-page.component';
+
+
 
 @NgModule({
   declarations: [
@@ -15,7 +19,7 @@ import { ErrorPageComponent } from './shared/error-page/error-page.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
